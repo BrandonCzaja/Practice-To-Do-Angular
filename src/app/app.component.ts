b71af780c34ed5a6693ff8c9c47c02c3a9771249
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-01-08 09:58:39 -0600

    Defined ToDos variable, fetched data from backend, called the initialized method with OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angtemplate';
+export class AppComponent implements OnInit {
+  todos: Array<any> = [];
+  baseUrl: string = 'http://localhost:3000/todos';
+
+  async getTodos() {
+    const response = await fetch(this.baseUrl);
+    const data = await response.json();
+    this.todos = await data;
+  }
+
+  ngOnInit() {
+    this.getTodos();
+  }
 }
