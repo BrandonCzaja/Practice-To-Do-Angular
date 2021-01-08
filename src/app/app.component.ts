0b3f9341e995ccb4443201844f282f372e7adb4c
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-01-08 10:19:34 -0600

    Creating To Dos
    added logic to app.components.ts for creating todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,37 @@ export class AppComponent implements OnInit {
   todos: Array<any> = [];
   baseUrl: string = 'http://localhost:3000/todos';
 
+  // Properties to Bind with Create Form
+  createTitle: string = '';
+  createBody: string = '';
+
+  // Function to get list of To Dos from backend
   async getTodos() {
     const response = await fetch(this.baseUrl);
     const data = await response.json();
     this.todos = await data;
   }
 
+  // Creates a new todo with data from the form input
+  async createTodo() {
+    await fetch(this.baseUrl, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: this.createTitle,
+        body: this.createBody,
+      }),
+    });
+
+    // Update the todo list and reset the form
+    this.getTodos();
+    this.createTitle = '';
+    this.createBody = '';
+  }
+
+  // This function runs when the component Loads (think React.useEffect)
   ngOnInit() {
     this.getTodos();
   }
