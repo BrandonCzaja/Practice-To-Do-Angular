1d3860914c5dc4e3c2c76a4e73337982bd1a37be
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-01-08 10:38:20 -0600

    Added editing logi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,11 @@ export class AppComponent implements OnInit {
   createTitle: string = '';
   createBody: string = '';
 
+  // Properties that Bind with Edit Form
+  editTitle: string = '';
+  editBody: string = '';
+  editId: number = 0;
+
   // Function to get list of To Dos from backend
   async getTodos() {
     const response = await fetch(this.baseUrl);
@@ -20,6 +25,9 @@ export class AppComponent implements OnInit {
     this.todos = await data;
   }
 
+  ///////////
+  // CREATE
+  //////////
   // Creates a new todo with data from the form input
   async createTodo() {
     await fetch(this.baseUrl, {
@@ -39,6 +47,34 @@ export class AppComponent implements OnInit {
     this.createBody = '';
   }
 
+  ////////
+  // EDIT
+  ////////
+  // Edit todos
+  editSelect(todo) {
+    this.editId = todo.id;
+    this.editTitle = todo.title;
+    this.editBody = todo.body;
+  }
+
+  // Updates the selected todo with form data
+  async updateTodo() {
+    await fetch(this.baseUrl + '/' + this.editId, {
+      method: 'put',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: this.editTitle,
+        body: this.editBody,
+      }),
+    });
+    // Update todo list with edited todo then reset the form
+    this.getTodos();
+    this.editTitle = '';
+    this.editBody = '';
+    this.editId = 0;
+  }
   // This function runs when the component Loads (think React.useEffect)
   ngOnInit() {
     this.getTodos();
